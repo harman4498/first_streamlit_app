3f63b25f91a4086ca90efa51ac179627b81aaff2
harman4498 <36203143+harman4498@users.noreply.github.com>
2022-05-16 13:05:32 -0500

    Update streamlit_app.py
    Added functionality to only display the fruits selected by customer

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,5 +14,6 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Let's put a pick list here so they can pick the fruite they want to include
-streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
-streamlit.dataframe(my_fruit_list)
+fruits_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
