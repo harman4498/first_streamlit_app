8e6d11a36f6094d3b416482ac38003dea390183b
harman4498 <36203143+harman4498@users.noreply.github.com>
2022-05-16 13:01:57 -0500

    Update streamlit_app.py
    Added example fruits to pick section

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,5 +14,5 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Let's put a pick list here so they can pick the fruite they want to include
-streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
 streamlit.dataframe(my_fruit_list)
